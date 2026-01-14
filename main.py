801552069835f695b22f60b19eee020b544b4ce6
luetsrr <ymstar4160@gmail.com>
2026-01-15 01:18:37 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,22 @@ from email.mime.text import MIMEText
 import os
 from dotenv import load_dotenv
 
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "I am alive!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
 # --- 設定の読み込み ---
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
